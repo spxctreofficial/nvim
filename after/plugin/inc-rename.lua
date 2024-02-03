b4de03f9d2fd61ee5b30c011344ac16a35271163
spxctreofficial <spxctre@spxct.cf>
2024-02-03 10:55:35 -0500

    Removed after/plugin directory
    Signed-off-by: spxctreofficial <spxctre@spxct.cf>

--- after/plugin/inc-rename.lua
+++
@@ -1,7 +0,0 @@
-local inc_rename = require("inc_rename")
-
-inc_rename.setup()
-
-vim.keymap.set("n", "<leader>r", function()
-    return ":IncRename " .. vim.fn.expand("<cword>")
-end, { expr = true })
