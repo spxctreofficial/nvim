5b3576b1366ed49cba323bb47a28687e660d5aab
spxctreofficial <spxctre@spxct.cf>
2024-04-25 13:42:10 -0400

    Telescope UI Integration with Harpoon

--- lua/spxctre/plugins/harpoon.lua
+++ lua/spxctre/plugins/harpoon.lua
@@ -2,6 +2,9 @@ return {
     "theprimeagen/harpoon",
     branch = "harpoon2",
     event = "VeryLazy",
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+    },
     opts = {
         global_settings = {
             save_on_toggle = true,
@@ -18,8 +21,28 @@ return {
         local harpoon = require('harpoon')
         harpoon.setup(opts)
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        -- Telescope UI setup with Harpoon
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers").new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table({
+                    results = file_paths,
+                }),
+                previewer = conf.file_previewer({}),
+                sorter = conf.generic_sorter({}),
+            }):find()
+        end
+
+        vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end, {
+        desc = "Open Harpoon UI"
+    })
     end
 
 }
