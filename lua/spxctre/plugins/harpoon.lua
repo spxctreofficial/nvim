9663365819cd28cc907a09c667364a49b0942c3f
spxctreofficial <spxctre@spxct.cf>
2024-05-03 22:30:24 -0400

    Harpoon2 Rounded UI, similar to Classic Harpoon
    Signed-off-by: spxctreofficial <spxctre@spxct.cf>

--- lua/spxctre/plugins/harpoon.lua
+++ lua/spxctre/plugins/harpoon.lua
@@ -7,17 +7,23 @@ return {
         "nvim-lua/plenary.nvim",
     },
     opts = {
-        settings = { 
+        settings = {
             save_on_toggle = true,
             sync_on_ui_close = true,
         },
     },
     config = function(_, opts)
         local harpoon = require('harpoon')
-        harpoon.setup(opts)
+        harpoon:setup(opts)
+
+        local toggle_opts = {
+            border = "rounded",
+            title_pos = "center",
+            ui_width_ratio = 0.40,
+        }
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list(), toggle_opts) end)
     end
 
 }
