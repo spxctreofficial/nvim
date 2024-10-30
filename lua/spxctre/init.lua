e606a2bbab6f6a176a718fc5f9d66cea7327ab1b
spxctreofficial <spxctre@spxct.cf>
2024-10-30 10:36:45 -0400

    Neovide support

--- lua/spxctre/init.lua
+++ lua/spxctre/init.lua
@@ -1,6 +1,10 @@
 require('spxctre.remap')
 require('spxctre.set')
 
+if vim.g.neovide then
+    require('spxctre.neovide')
+end
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
