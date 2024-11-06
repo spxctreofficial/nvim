ae12ac708417ccc9252aeed25208967d0c8fa0e5
spxctreofficial <spxctre@spxct.cf>
2024-11-06 08:06:15 -0500

    Web development support with ESLint, typescript-language-server, Volar (for vue), updated Treesitter languages, removed automatic LSP setup

---
+++ lua/spxctre/plugins/nvim-ts-autotag.lua
@@ -0,0 +1,15 @@
+return {
+    'windwp/nvim-ts-autotag',
+    ft = {
+        'javascript',
+        'javascriptreact',
+        'typescript',
+        'typescriptreact',
+        'html',
+        'vue',
+    },
+    event = VeryLazy,
+    config = function()
+        require('nvim-ts-autotag').setup()
+    end
+}
