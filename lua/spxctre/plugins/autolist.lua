b674d6ffe51120ee9704ceec06155263a90afab6
spxctreofficial <spxctre@spxct.cf>
2025-04-14 14:41:38 -0400

    Fix `autolist.lua` bindings not working when opening single file

--- lua/spxctre/plugins/autolist.lua
+++ lua/spxctre/plugins/autolist.lua
@@ -1,27 +1,30 @@
 return {
-    "gaoDean/autolist.nvim", -- Autocomplete lists
+    "gaoDean/autolist.nvim",
     event = "VeryLazy",
     config = function()
         require("autolist").setup()
 
-        local function bind_keys(bufnr)
-            local opts = { buffer = bufnr, remap = false }
+        vim.keymap.set("i", "<tab>", "<cmd>AutolistTab<cr>")
+        vim.keymap.set("i", "<s-tab>", "<cmd>AutolistShiftTab<cr>")
+        -- vim.keymap.set("i", "<c-t>", "<c-t><cmd>AutolistRecalculate<cr>") -- an example of using <c-t> to indent
+        vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+        vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+        vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+        vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+        vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
 
-            vim.keymap.set('i', '<tab>', '<cmd>AutolistTab<cr>', opts)
-            vim.keymap.set('i', '<s-tab>', '<cmd>AutolistShiftTab<cr>', opts)
-            vim.keymap.set('i', '<CR>', '<CR><cmd>AutolistNewBullet<cr>', opts)
+        -- cycle list types with dot-repeat
+        vim.keymap.set("n", "<leader>cn", require("autolist").cycle_next_dr, { expr = true })
+        vim.keymap.set("n", "<leader>cp", require("autolist").cycle_prev_dr, { expr = true })
 
-            vim.keymap.set('n', 'o', 'o<cmd>AutolistNewBullet<cr>', opts)
-            vim.keymap.set('n', 'O', 'O<cmd>AutolistNewBulletBefore<cr>', opts)
-            vim.keymap.set('n', '<CR>', '<cmd>AutolistToggleCheckbox<cr>j', opts)
-            vim.keymap.set('n', '<S-CR>', '<cmd>AutolistToggleCheckbox<cr>k', opts)
-            -- vim.keymap.set('n', '<C-r>', '<cmd>AutolistRecalculate<cr>', opts)
-        end
-        vim.api.nvim_create_autocmd('BufEnter', {
-            pattern = { '*.md', '*.txt' },
-            callback = function(args)
-                bind_keys(args.buf) -- Key mappings when an LSP is attached to a buffer.
-            end
-        })
-    end
+        -- if you don't want dot-repeat
+        -- vim.keymap.set("n", "<leader>cn", "<cmd>AutolistCycleNext<cr>")
+        -- vim.keymap.set("n", "<leader>cp", "<cmd>AutolistCycleNext<cr>")
+
+        -- functions to recalculate list on edit
+        vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+        vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+        vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+        vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+    end,
 }
