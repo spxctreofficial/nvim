9da96b1e20c0f9d1774efbf4c6a4ca66fe090c97
spxctreofficial <spxctre@spxct.cf>
2025-04-14 15:53:04 -0400

    Markdown highlighting with `render-markdown.nvim`

---
+++ lua/spxctre/plugins/live-preview.lua
@@ -0,0 +1,15 @@
+return {
+    "brianhuster/live-preview.nvim",
+    dependencies = "nvim-telescope/telescope.nvim",
+    event = "VeryLazy",
+    opts = {
+        port = 5500,
+        browser = "default",
+        dyanmic_root = false,
+        sync_scroll = true,
+        picker = "telescope",
+    },
+    config = function(opts)
+        require("livepreview.config").set(opts)
+    end
+}
---
+++ lua/spxctre/plugins/markdown.lua
@@ -0,0 +1,17 @@
+return {
+    'MeanderingProgrammer/render-markdown.nvim',
+    event = "VeryLazy",
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        completions = {
+            blink = {
+                enabled = true,
+            },
+        },
+    },
+    config = function(opts)
+        require('render-markdown').setup(opts)
+    end
+}
