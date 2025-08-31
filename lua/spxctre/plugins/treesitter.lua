1cb82d45ff8752c8eea82d179f91530ce49f5af0
spxctreofficial <onlyspxctre@proton.me>
2025-08-30 20:32:30 -0400

    Migrated to  rewrite

--- lua/spxctre/plugins/treesitter.lua
+++ lua/spxctre/plugins/treesitter.lua
@@ -2,30 +2,15 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = "VeryLazy",
+    branch = "main",
     opts = {
-        ensure_installed = { 'c', 'cpp', 'rust', 'markdown', 'javascript', 'typescript', 'tsx', 'json' },
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-        auto_install = false,
-
-        ignore_install = {},
-        modules = {},
-
-        highlight = {
-            enable = true,
-            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-            -- Using this option may slow down your editor, and you may see some duplicate highlights.
-            -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
-        },
+        parsers = { "cpp", "rust", "zig", "typescript", "javascript", "tsx", "jsx" }, -- C, Lua, Markdown is preinstalled
     },
-
     config = function(_, opts)
-        require('nvim-treesitter.configs').setup(opts)
+        vim.api.nvim_create_user_command("TSGetParsers", function ()
+            for _, p in ipairs(opts.parsers) do
+                vim.cmd("TSInstall " .. p)
+            end
+        end, {})
     end
 }
--- lua/spxctre/window.lua
+++ lua/spxctre/window.lua
@@ -17,3 +17,10 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
         if vim.o.filetype == "help" then vim.cmd.wincmd("L") end
     end
 })
+
+vim.api.nvim_create_autocmd('filetype', {
+    pattern = require("spxctre.plugins.treesitter").opts.parsers,
+    callback = function()
+        vim.treesitter.start()
+    end,
+})
